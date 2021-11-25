bc3153d3822802dfe9a4b717fa706945f168da64
Harun ERGUL <harunergul@outlook.com>
2021-11-25 14:56:08 +0300

    Adding patientdto and related DDL

---
+++ shared/src/main/java/com/erc/his/entity/PatientDTO.java
@@ -0,0 +1,72 @@
+package com.erc.his.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class PatientDTO implements Serializable {
+
+	private Long patientId;
+	private String identificationNo;
+	private String firstName;
+	private String lastName;
+	private Date birthDate;
+	private String gender;
+	private String status;
+
+	public Long getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(Long patientId) {
+		this.patientId = patientId;
+	}
+
+	public String getIdentificationNo() {
+		return identificationNo;
+	}
+
+	public void setIdentificationNo(String identificationNo) {
+		this.identificationNo = identificationNo;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
