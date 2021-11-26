1b0643fa287c7c3f02153960811aaf5e6bfeb404
Harun ERGUL <harunergul@outlook.com>
2021-11-26 13:25:09 +0300

    install.bat added

---
+++ install.bat
@@ -0,0 +1,5 @@
+CALL  mvn install:install-file -Dfile=./jars/jdatepicker-1.3.2.jar -DgroupId=jdp -DartifactId=jdatepicker -Dversion=1.3.2 -Dpackaging=jar
+CALL  mvn install:install-file -Dfile=./jars/jdatepicker-1.3.4-javadoc.jar -DgroupId=jdp -DartifactId=jdatepicker-javadoc -Dversion=1.3.4 -Dpackaging=jar
+CALL  mvn install:install-file -Dfile=./jars/jide-grids-3.5.15.jar -DgroupId=com.jidesoft -DartifactId=jide-grids -Dversion=3.5.15 -Dpackaging=jar
+CALL  mvn install:install-file -Dfile=./jars/jide-oss-3.6.18.jar -DgroupId=com.jidesoft -DartifactId=jide-oss -Dversion=3.6.18 -Dpackaging=jar
+CALL  mvn install:install-file -Dfile=./jars/jide-components-3.5.15.jar -DgroupId=com.jidesoft -DartifactId=jide-components -Dversion=3.5.15 -Dpackaging=jar
