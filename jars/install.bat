58cf3bf3e613cf7fd1f775a511ffa032ca824656
Harun ERGUL <harunergul@outlook.com>
2021-11-26 13:17:56 +0300

    Dependency fixes

---
+++ jars/install.bat
@@ -0,0 +1,5 @@
+CALL  mvn install:install-file -Dfile=jdatepicker-1.3.2.jar -DgroupId=jdp -DartifactId=jdatepicker -Dversion=1.3.2 -Dpackaging=jar
+CALL  mvn install:install-file -Dfile=jdatepicker-1.3.4-javadoc.jar -DgroupId=jdp -DartifactId=jdatepicker-javadoc -Dversion=1.3.4 -Dpackaging=jar
+CALL  mvn install:install-file -Dfile=jide-grids-3.5.15.jar -DgroupId=com.jidesoft -DartifactId=jide-grids -Dversion=3.5.15 -Dpackaging=jar
+CALL  mvn install:install-file -Dfile=jide-oss-3.6.18.jar -DgroupId=com.jidesoft -DartifactId=jide-oss -Dversion=3.6.18 -Dpackaging=jar
+CALL  mvn install:install-file -Dfile=jide-components-3.5.15.jar -DgroupId=com.jidesoft -DartifactId=jide-components -Dversion=3.5.15 -Dpackaging=jar
