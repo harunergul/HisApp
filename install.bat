07678626b900e8f0c051ade9c7f26a5abd1b5a41
Harun ERGUL <harunergul@outlook.com>
2021-12-13 12:30:43 +0300

    adding jcalendar as dependency

--- install.bat
+++ install.bat
@@ -3,3 +3,5 @@ CALL  mvn install:install-file -Dfile=./jars/jdatepicker-1.3.4-javadoc.jar -Dgro
 CALL  mvn install:install-file -Dfile=./jars/jide-grids-3.5.15.jar -DgroupId=com.jidesoft -DartifactId=jide-grids -Dversion=3.5.15 -Dpackaging=jar
 CALL  mvn install:install-file -Dfile=./jars/jide-oss-3.6.18.jar -DgroupId=com.jidesoft -DartifactId=jide-oss -Dversion=3.6.18 -Dpackaging=jar
 CALL  mvn install:install-file -Dfile=./jars/jide-components-3.5.15.jar -DgroupId=com.jidesoft -DartifactId=jide-components -Dversion=3.5.15 -Dpackaging=jar
+CALL  mvn install:install-file -Dfile=./jars/jcalendar-1.4.jar -DgroupId=com.jcalendar -DartifactId=jcalendar -Dversion=1.4 -Dpackaging=jar
+
